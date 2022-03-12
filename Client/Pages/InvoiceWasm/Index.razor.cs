b41bfff2a5bdb4d88510a39e4dbeabb63db69ff7
Ashley Marques <marques.ashley@gmail.com>
2022-03-12 09:42:55 -0300

    changed pages ids

--- Client/Pages/InvoiceWasm/Index.razor.cs
+++ Client/Pages/InvoiceWasm/Index.razor.cs
@@ -1,13 +1,90 @@
 
 
+using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Reactive.Linq;
+using InvoiceSamurai.Client.Config;
 using InvoiceSamurai.Client.Shared;
+using InvoiceSamurai.Shared;
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 namespace InvoiceSamurai.Client.Pages.InvoiceWasm;
 
 [Route("/invoice-wasm")]
 [Layout(typeof(InvoiceLayout))]
-public partial class Index: ComponentBase
+public partial class Index : ComponentBase
 {
 
+    [Inject] HttpClient httpClient { get; set; }
+    [Inject] IJSRuntime JSRuntime { get; set; }
+    protected enum UpdateFieldOn
+    {
+        Keystroke,
+        LostFocus
+    }
+    protected event EventHandler<int> RaiseNewHashCode;
+    protected InvoiceModel Invoice { get; set; } = new();
+    protected byte[] PdfBody = Array.Empty<byte>();
+
+    protected CustomerModel Customer = new CustomerModel();
+    protected GeneratePdfCommand pdfCommand = new GeneratePdfCommand();
+    protected UpdateFieldOn TextFieldInput { get; set; } = UpdateFieldOn.Keystroke; // defualt
+    protected string UpdateOnText => TextFieldInput == UpdateFieldOn.Keystroke ? "oninput" : "onchange";
+    string InvoiceHeader => Customer.IsNull() ? "Invoice" : $"Invoice for {Customer?.Name}";
+    protected override void OnInitialized()
+    {
+        object p = Observable.FromEventPattern<int>(h => RaiseNewHashCode += h, h => RaiseNewHashCode -= h)
+            .Throttle(TimeSpan.FromMilliseconds(600))
+            .Select(async (c) => new { response = await httpClient.PostAsJsonAsync("/pdfinvoice", pdfCommand) })
+                .Concat()
+             .Where(c => c.response.StatusCode == System.Net.HttpStatusCode.Created)
+             .Select(async (c) => new { content = await c.response.Content.ReadAsByteArrayAsync() })
+             .Concat()
+             .Select(c => c.content)
+            .Subscribe(HandleIncomingPdf);
+
+
+    }
+    protected void SetUpdateSettings(UpdateFieldOn newValue)
+    {
+        TextFieldInput = newValue;
+    }
+
+
+    private void HandleIncomingPdf(byte[] pdfBody)
+    {
+        PdfBody = pdfBody;    
+        JSRuntime.InvokeVoidAsync("PdfRenderer.renderPdf", pdfBody);
+        StateHasChanged();
+    }
+
+
+
+
+    int previousCommand = -1;
+    protected void RenderPdf()
+    {
+        pdfCommand = pdfCommand with
+        {
+            Invoice = Invoice,
+            Customer = Customer
+        };
+        if (pdfCommand.GetHashCode() == previousCommand)
+        {
+            return;
+        }
+        previousCommand = pdfCommand.GetHashCode();
+        RaiseNewHashCode?.Invoke(this, previousCommand);
+
+        PdfBody = Array.Empty<byte>();
+        _ = JSRuntime?.InvokeVoidAsync("PdfRenderer.clearCanvas");
+
+
+    }
+
+
+
 }
+
